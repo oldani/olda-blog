d97bce2d7302dc7f4777e495aebd710c21f2484e
oldanis <ordanisanchez@gmail.com>
2016-03-28 22:29:53 -0400

    sign up validator complete

--- estatico/login.js
+++ estatico/login.js
@@ -1,37 +1,75 @@
 
 var regexPw= /(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])\S{6,}$/;
 
-var validarN= function(){procesarSolicitud("#username",/^[\w]{4,12}$/);
+var validarN= function(){procesarSolicitud("#usernameS",/^[\w]{4,12}$/);
 };
 
-var validarPw = function(){procesarSolicitud("#pass", regexPw);
+var validarPw = function(){procesarSolicitud("#passS", regexPw);
 };
 
-var confirmarPw= function() {procesarSolicitud("#passV", regexPw);
+var confirmarPw= function() {procesarSolicitud("#passVs", regexPw);
 };
 
 var validarMail= function() {procesarSolicitud("#correo", /^[\S]+@[\S]+\.[\S]+$/);
 };
 
+var usernameWrong, pwWrong, pwNotMatch, mailWrong
+
+usernameWrong= "<span class=help-block>Your Username must have at least of 4 caracters</span>";
+pwWrong= "<span class=help-block>Your Password must have at least of 1 Lowercase, 1 Uppercase letters and 1 number</span>";
+pwNotMatch="<span class=help-block>Your Password did not match</span>";
+mailWrong="<span class=help-block>Please enter a valid Email</span>";
+
+var campos=0;
+console.log(campos);
+
 function procesarSolicitud(sel, regex) {
-	$(sel).blur(function(){
-		var valor, formGroup 
-		valor= $(this).val();
-		formGroup= $(this).closest(".form-group");
-		
-
+	var formGroup= $(sel).closest(".form-group");
+	$(sel).keyup(function() {
+		var valor= $(this).val();
+		var errorMsg= $(formGroup).find("span").hasClass("help-block");
 		if (valor.match(regex)) {
-
-			if (sel=="#passV" && valor!= $("#pass").val()) {
-				$(formGroup).addClass("has-error");
+			if (sel=="#passVs" && valor!= $("#passS").val()) {
+				if (!errorMsg) {
+					$(formGroup).addClass("has-error");
+					$(this).parent().after(pwNotMatch);
+				};
 			} else {
 				$(formGroup).removeClass("has-error");
+				$(formGroup).children(".help-block").remove();
 				$(formGroup).addClass("has-success");
 			};
-		} else {
-			$(formGroup).addClass("has-error")
+		} else if (sel!="#passVs") {
+			$(formGroup).removeClass("has-success");
+			$(formGroup).addClass("has-error");
+			if (sel=="#usernameS" && !errorMsg) {
+				$(this).parent().after(usernameWrong);
+			} else if (sel=="#passS" && !errorMsg) {
+				$(this).parent().after(pwWrong)
+			} else if (sel=="#correo" && !errorMsg) {
+				$(this).parent().after(mailWrong);
+			};
 		};
 
+		// var valido= $(formGroup).hasClass("has-success");
+		// if (valido) {
+		// 	campos+=1;
+		// 	if(valido && campos>4){
+		// 		campos-=1;
+		// 	}
+		// 	console.log(campos);
+		// };
+
+		// if (campos>=4) {
+		// 	$("#submit").prop("disabled", false);
+		// };
+		
+		
 	});
+	
+
+
 }
+
+
 
