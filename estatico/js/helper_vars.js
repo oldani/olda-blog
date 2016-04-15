d1866a8d5102e2d7b2a46e03c040ddc4c4599cb4
oldanis <ordanisanchez@gmail.com>
2016-04-15 15:59:01 -0400

    add form y maquetacion para comentarios, completa la ajax request para la misma, modifica el helper file

--- estatico/js/helper_vars.js
+++ estatico/js/helper_vars.js
@@ -42,3 +42,14 @@ var makeBlink={
 
 };
 var intervalo= setInterval(makeBlink.blinker, 1000);
+
+// error para comentarios
+var errorComentario, cerrarComentario
+errorComentario= //'<div class="row">\
+					//'<div class="col-xs-10 col-sm-5 col-md-4 col-lg-4">\
+						'<div class="alert alert-danger alert-dismissible" role=" alert" \
+							style="margin-top:10px">\
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+								<span aria-hidden="true">&times;</span>\
+							</button>';
+cerrarComentario='</div>';//</div>';//</div>';
